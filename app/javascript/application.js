908ce96464bb8a67b51d40f4bc7875d4b9e1cc11
adnan <you@example.com>
2022-09-13 16:56:47 +0530

    Navbaradd

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,18 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+
+
+document.querySelectorAll('.dropdown-toggle').forEach(item => {
+  item.addEventListener('click', event => {
+ 
+    if(event.target.classList.contains('dropdown-toggle') ){
+      event.target.classList.toggle('toggle-change');
+    }
+    else if(event.target.parentElement.classList.contains('dropdown-toggle')){
+      event.target.parentElement.classList.toggle('toggle-change');
+    }
+  })
+});
+
+
